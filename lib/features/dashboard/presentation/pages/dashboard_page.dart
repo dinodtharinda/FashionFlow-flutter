c8ed7cad2cd8dbc06485dc6f4bf0cd41e5829ef8
Dinod Tharinda <94352564+dinodtharinda@users.noreply.github.com>
2024-10-13 19:47:28 +0530

    add navigation bar

--- lib/features/dashboard/presentation/pages/dashboard_page.dart
+++ lib/features/dashboard/presentation/pages/dashboard_page.dart
@@ -1,33 +1,51 @@
 import 'package:fashion_flow/core/common/animations/custom_page_route.dart';
-import 'package:fashion_flow/features/auth/presentation/bloc/auth_bloc.dart';
-import 'package:fashion_flow/features/auth/presentation/pages/auth_option_page.dart';
+import 'package:fashion_flow/features/product/presentation/pages/products_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-class DashBoardPage extends StatelessWidget {
+class DashBoardPage extends StatefulWidget {
   static route() => CustomePageRoute(
         child: const DashBoardPage(),
       );
   const DashBoardPage({super.key});
 
   @override
+  State<DashBoardPage> createState() => _DashBoardPageState();
+}
+
+class _DashBoardPageState extends State<DashBoardPage> {
+  int _pageIndex = 0;
+
+   final List<Widget> _pages = [
+    const ProductsPage(),
+   const Text("store"),
+   const Text("wishlist"),
+   const Text("settings"),
+
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('home'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              context.read<AuthBloc>().add(
-                    AuthLogout(),
-                  );
-              Navigator.pushAndRemoveUntil(
-                  context, AuthOptionPage.route(), (route) => false);
-            },
-            icon: const Icon(Icons.logout),
-          )
+      bottomNavigationBar: NavigationBar(
+        height: 80,
+        elevation: 0,
+        selectedIndex: _pageIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            _pageIndex = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'home'),
+          NavigationDestination(
+              icon: Icon(Icons.storefront_sharp), label: 'Store'),
+          NavigationDestination(
+              icon: Icon(Icons.favorite_border), label: 'Wishlist'),
+          NavigationDestination(
+              icon: Icon(Icons.settings_outlined), label: 'settings'),
         ],
       ),
+      body: Center(child: _pages[_pageIndex]),
     );
+    
   }
 }
